7d23b71409dbbcbe4c967ffa4e85aa65ad514741
HirendraJyani <hirendrajyani9@gmail.com>
2024-01-08 15:25:13 +0530

    new changes added

--- src/Footer.js
+++ src/Footer.js
@@ -1,27 +1,44 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from "react";
 import "./Footer.css";
-import AOS from 'aos';
+import AOS from "aos";
 import "aos/dist/aos.css";
 
 function Footer() {
-    useEffect(() => {
-        AOS.init();
-        AOS.refresh();
-      }, []);
-    return (
-        <div className="footer" >
-            <div className="footer_icons" >
-            <a className="footer_icon" rel="noreferrer" href="https://www.linkedin.com/in/hirendra-jat-277b041a7/" target="_blank"> <i className="fab fa-linkedin fa-lg "></i> </a>
-            <a className="footer_icon" rel="noreferrer" href="https://github.com/HirendraJyani" target="_blank"> <i className="fab fa-github fa-lg "></i> </a>
-            <a href="./resume/Hirendra_Resume.pdf" rel="noreferrer" target="_blank">
-                <i className="far fa-file fa-lg footer_icon"></i>
-            </a>
-            </div><br/>
-            <div className="footer_message">
-                <p>Copyright - 2021 Hirendra Jat, All Right Reserved</p>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+  return (
+    <div className="footer">
+      <div className="footer_icons">
+        <a
+          className="footer_icon"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/hirendra-jat-277b041a7/"
+          target="_blank"
+        >
+          {" "}
+          <i className="fab fa-linkedin fa-lg "></i>{" "}
+        </a>
+        <a
+          className="footer_icon"
+          rel="noreferrer"
+          href="https://github.com/HirendraJyani"
+          target="_blank"
+        >
+          {" "}
+          <i className="fab fa-github fa-lg "></i>{" "}
+        </a>
+        <a href="./resume/Hirendra_Resume.pdf" rel="noreferrer" target="_blank">
+          <i className="far fa-file fa-lg footer_icon"></i>
+        </a>
+      </div>
+      <br />
+      <div className="footer_message">
+        <p>Copyright - 2024 Hirendra Jat, All Right Reserved</p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- src/Middle.js
+++ src/Middle.js
@@ -1,43 +1,85 @@
-import React from 'react'
+import React from "react";
 import "./Middle.css";
-import AOS from 'aos';
+import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 
 function Middle() {
-    useEffect(() => {
-        AOS.init();
-        AOS.refresh();
-      }, []);
-    return (
-        <div className="middle_screen" data-aos={"zoom-in"}>
-        <div id="about" className="middle">
-            <div className="my_profile">
-                <img className="my_pic" src="./images/h.jpg" alt="MyPic"/>
-            </div>
-            <div className="about_me">
-                <h1>Hi! I'm Hirendra Jat, <br/> A Full Stack Developer <br/> From Ajmer, Raj., India.</h1> <br/>
-                <p>I am a Full stack developer. I am a programmer with good knowledge of front & back-end techniques.
-                   I love structure and order and I also stand for quality. I love spending time on fixing little details
-                   and optimizing web apps. Also I like working in a team.
-                </p>
-                <br/>
-                <h3>Let's build something cool</h3><br/>
-                <p><span className="profile_links">Mail:</span> <a className="project_links" href="mailto:itshirendra@gmail.com" > itshirendra@gmail.com </a></p><br/>
-                <p><span className="profile_links">LinkedIn:</span> <a className="project_links" href="https://www.linkedin.com/in/hirendra-jat-277b041a7/" rel="noreferrer" target="_blank"> https://www.linkedin.com/in/hirendra-jat-277b041a7/ </a></p><br/>
-                <p><span className="profile_links">Github:</span> <a className="project_links" href="https://github.com/HirendraJyani" rel="noreferrer" target="_blank"> https://github.com/HirendraJyani </a></p><br/>
-                <a href="./resume/Hirendra_Resume.pdf" rel="noreferrer" target="_blank">
-                <button className="profile__button">RESUME</button>
-                </a>
-            </div>
-            
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+  return (
+    <div className="middle_screen" data-aos={"zoom-in"}>
+      <div id="about" className="middle">
+        <div className="my_profile">
+          <img className="my_pic" src="./images/profile-pic.png" alt="MyPic" />
         </div>
-        <hr/>
-        {/* <script>
+        <div className="about_me">
+          <h1>
+            Hi! I'm Hirendra Jat, <br /> A Full Stack Developer <br /> From
+            Rajasthan, India.
+          </h1>{" "}
+          <br />
+          <p>
+            Graduate of computer science with 2.5 years of experience working
+            across Frontend and Backend web development. I love spending time on
+            fixing little details and optimizing web apps. Also I like working
+            in a team. I’m looking for a role where I can grow and learn from
+            other experienced team members.
+          </p>
+          <br />
+          <h3>Let's build something cool</h3>
+          <br />
+          <p>
+            <span className="profile_links">Mail:</span>{" "}
+            <a className="project_links" href="mailto:itshirendra@gmail.com">
+              {" "}
+              itshirendra@gmail.com{" "}
+            </a>
+          </p>
+          <br />
+          <p>
+            <span className="profile_links">LinkedIn:</span>{" "}
+            <a
+              className="project_links"
+              href="https://www.linkedin.com/in/hirendra-jat-277b041a7/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              {" "}
+              https://www.linkedin.com/in/hirendra-jat-277b041a7/{" "}
+            </a>
+          </p>
+          <br />
+          <p>
+            <span className="profile_links">Github:</span>{" "}
+            <a
+              className="project_links"
+              href="https://github.com/HirendraJyani"
+              rel="noreferrer"
+              target="_blank"
+            >
+              {" "}
+              https://github.com/HirendraJyani{" "}
+            </a>
+          </p>
+          <br />
+          <a
+            href="./resume/Hirendra_Resume.pdf"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <button className="profile__button">RESUME</button>
+          </a>
+        </div>
+      </div>
+      <hr />
+      {/* <script>
       AOS.init();
     </script> */}
-        </div>
-    );
+    </div>
+  );
 }
 
 export default Middle;
